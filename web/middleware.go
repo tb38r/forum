be1f41d4294458a27d54a1dc5e59506097f95a1f
nsym_coding <nsym.coding@outlook.com>
2022-04-26 12:18:09 +0100

    bit of progress

--- web/middleware.go
+++ web/middleware.go
@@ -1,6 +1,7 @@
 package web
 
 import (
+	"fmt"
 	"forum/users"
 	"net/http"
 )
@@ -8,11 +9,19 @@ import (
 func Auth(HandlerFunc http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 
-		_, user := users.DbSessions[users.CurrentUser]
-		if !user {
+		c, err := r.Cookie(users.CurrentUser)
+		fmt.Println(c.Value)
+		if err != nil {
 			http.Redirect(w, r, "/login", http.StatusSeeOther)
 			return
 		}
+
+		// _, user := users.DbSessions[users.CurrentUser]
+		// if !user {
+		// 	fmt.Println("Checking loop ------------------->", users.CurrentUser)
+		// if !users.SessionExists(users.CurrentUser) {
+
+		// }
 		HandlerFunc.ServeHTTP(w, r)
 	}
 }
