c8eaa5b60c7216b7238e23dcbbcf1482ced66d69
eternal17 <sarmadkhatri15@gmail.com>
2022-04-28 13:17:59 +0100

    homepage displaying username if logged in, and login form if not logged in

--- web/handleGeneral.go
+++ web/handleGeneral.go
@@ -2,20 +2,25 @@ package web
 
 import (
 	"fmt"
+	"forum/users"
 	"net/http"
 )
 
 // eventually need this struct to pass onto the handler with all the data within
-// type HomepageData struct {
-// 	// allPostTitles []string
-// 	Loggedin?  bool
-
-// }
+// struct fields need to be capitalized, to be used in the templates
+type HomepageData struct {
+	Username string
+	// allPostTitles []string
+	Loggedin bool
+}
 
 // in chrome this handler is being run twice on localhost:8080, on safari only once (which is what we need) *** UNLESS route is changed from / to /home
 func (s *Server) HomepageHandler() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("homepage handler running")
-		Tpl.ExecuteTemplate(w, "homepage.html", nil)
+		// checking if user is logged in
+		user := users.CurrentUser
+		homePageData := HomepageData{user, users.AlreadyLoggedIn(r)}
+		Tpl.ExecuteTemplate(w, "homepage.html", homePageData)
 	}
 }
