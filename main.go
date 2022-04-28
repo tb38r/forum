b8947191f3928640afb5f8b393d671abee4a1299
tb38r <tlawal86@hotmail.co.uk>
2022-04-28 13:42:38 +0100

    readyformerge

--- main.go
+++ main.go
@@ -3,11 +3,20 @@ package main
 import (
 	"forum/database"
 	"forum/web"
+	"time"
 
 	_ "github.com/mattn/go-sqlite3"
 )
 
+var (
+	limiter <-chan time.Time
+	seconds time.Duration
+)
+
 func main() {
+	seconds = 2
+	limiter = time.Tick(seconds * time.Second)
+
 	database.CreateDB()
-	web.OpenServer()
+	web.OpenServer(limiter)
 }
