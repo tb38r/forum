40167829571b1395edeeb653520c9878a533a925
nsym_coding <nsym.coding@outlook.com>
2022-05-04 00:55:00 +0100

    showing comment on store comment handler and linking user id/post id

--- web/handleComments.go
+++ web/handleComments.go
@@ -35,8 +35,18 @@ func (s *myServer) StoreCommentHandler() http.HandlerFunc {
 
 		comments.CreateComment(s.Db, GuserId, PostIDInt, ContentComment)
 
+		var commentData comments.Comment
+
+		commentData.CommentText = ContentComment
+		commentData.PostID = PostIDInt
+		commentData.UserID = GuserId
+
+		fmt.Println("comment data check: ---> ", commentData.CommentText)
+		fmt.Println("comment post id check: ---> ", commentData.PostID)
+		fmt.Println("comment user id check: ---> ", commentData.UserID)
+
 		fmt.Println("content: ", ContentComment)
-		Tpl.ExecuteTemplate(w, "storecomment.html", nil)
+		Tpl.ExecuteTemplate(w, "storecomment.html", commentData.CommentText)
 
 	}
 }
@@ -47,9 +57,19 @@ func (s *myServer) ShowCommentHandler() http.HandlerFunc {
 		// had to open the database here as it wasnt picking the correct post everytime without this.
 		s.Db, _ = sql.Open("sqlite3", "forum.db")
 		// get the postId and display the post and its contents
-		// postID := r.URL.Query().Get("postid")
-		// PostIDInt, _ = strconv.Atoi(postID)
-		fmt.Println(comments.Comment{PostID: PostIDInt})
-		Tpl.ExecuteTemplate(w, "showcomment.html", comments.GetCommentData(s.Db, PostIDInt))
+		postID := r.URL.Query().Get("postid")
+		PostIDInt, _ = strconv.Atoi(postID)
+
+		var commentData comments.Comment
+
+		if PostIDInt == commentData.PostID {
+			commentData.CommentText = ContentComment
+			commentData.PostID = PostIDInt
+			commentData.UserID = GuserId
+		}
+		fmt.Println("comment data check: ---> ", commentData.CommentText)
+		fmt.Println("comment post id check: ---> ", commentData.PostID)
+
+		Tpl.ExecuteTemplate(w, "showcomment.html", commentData)
 	}
 }
