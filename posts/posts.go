a49d6731d3de359c97bd9e7e8b5a33abcf1fcad4
tb38r <tlawal86@hotmail.co.uk>
2022-05-06 18:04:36 +0100

    filter

--- posts/posts.go
+++ posts/posts.go
@@ -65,6 +65,26 @@ func GetHomepageData(db *sql.DB) []HomepagePosts {
 
 }
 
+// Gets data based on user's filtered choicee
+func FilterHomepageData(db *sql.DB, userID int) []HomepagePosts {
+	rows, err := db.Query("SELECT postID, postTitle, username, creationDate FROM post INNER JOIN users ON users.userID = ?", userID)
+	if err != nil {
+		fmt.Println(err)
+	}
+	postdata := []HomepagePosts{}
+	defer rows.Close()
+	for rows.Next() {
+		var p HomepagePosts
+		err2 := rows.Scan(&p.PostID, &p.PostTitle, &p.PostUsername, &p.CreationDate)
+		postdata = append(postdata, p)
+		if err2 != nil {
+			fmt.Println(err2)
+		}
+	}
+	return postdata
+
+}
+
 // getting the data from one post, and storing the values in the post struct
 func GetPostData(db *sql.DB, postID int) Post {
 	row := db.QueryRow("SELECT * FROM post WHERE postID = ?;", postID)
